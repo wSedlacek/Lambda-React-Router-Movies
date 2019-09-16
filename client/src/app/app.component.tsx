d8780d710a5ac7d3aa61f001c6b563d68d99ebb1
William Sedlacek <wsedlacekc@gmail.com>
2019-09-16 15:28:49 -0500

    Setup Routes

--- client/src/app/app.component.tsx
+++ client/src/app/app.component.tsx
@@ -1,8 +1,11 @@
 import React, { useState } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import { SavedList } from './components/saved-list/saved-list.component';
+import { MovieList } from './components/movie-list/movie-list.component';
 
 import { Movie } from './model/Movie';
+import { MoviePage } from './components/movie-page/movie-page.component';
 
 const App = () => {
   const [savedList, setSavedList] = useState<Movie[]>([]);
@@ -12,10 +15,13 @@ const App = () => {
   };
 
   return (
-    <div>
+    <Router>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
-    </div>
+      <Switch>
+        <Route exact path='/' component={MovieList} />
+        <Route path='/movies/:id' component={MoviePage} />
+      </Switch>
+    </Router>
   );
 };
 
